f74c783a63222e2e9a395314877ad7923228dfb2
yordan89k <54133275+yordan89k@users.noreply.github.com>
2019-08-26 14:52:26 +0200

    Installed EntityFramework, created localhostDb with tables trough PackageManagerConsole

---
+++ MyShopYK/MyShopYK.DataAccess.SQL/DataContext.cs
@@ -0,0 +1,23 @@
+﻿using MyShopYK.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyShopYK.DataAccess.SQL
+{
+    public class DataContext : DbContext
+    {
+        public DataContext()
+            : base("DefaultConnection")
+        {
+
+        }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<ProductCategory> ProductCategories { get; set; }
+
+    }
+}
