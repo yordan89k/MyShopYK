99fa89ae7b75ff6c650cb86f1cffb6402ea1c81e
yordan89k <54133275+yordan89k@users.noreply.github.com>
2019-08-27 11:00:13 +0200

    Connecting to SQL data base

--- MyShopYK/MyShopYK.WebUI/App_Start/UnityConfig.cs
+++ MyShopYK/MyShopYK.WebUI/App_Start/UnityConfig.cs
@@ -1,6 +1,7 @@
 using MyShopYK.Core.Contracts;
 using MyShopYK.Core.Models;
 using MyShopYK.DataAccess.InMemory;
+using MyShopYK.DataAccess.SQL;
 using System;
 
 using Unity;
@@ -45,8 +46,8 @@ namespace MyShopYK.WebUI
 
             // TODO: Register your type's mappings here.
             // container.RegisterType<IProductRepository, ProductRepository>();
-            container.RegisterType<IRepository<Product>, InMemoryRepository<Product>>();
-            container.RegisterType<IRepository<ProductCategory>, InMemoryRepository<ProductCategory>>();
+            container.RegisterType<IRepository<Product>, SQLRepository<Product>>();
+            container.RegisterType<IRepository<ProductCategory>, SQLRepository<ProductCategory>>();
         }   
     }
 }
